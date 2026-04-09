3696706b76c8cd82b404757bf3f000a8a5c98f94
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-09 15:10:19 +0200

    ADD array di oggetti per bici

--- script.js
+++ script.js
@@ -3,3 +3,42 @@
 //     Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // - Stampare a schermo la bici con peso minore.
 
+const biciDaCorsa = [
+    { 
+        nome: "Specialized S-Works Tarmac SL8", 
+        peso: 6.6 
+    },
+    { 
+        nome: "Trek Emonda SLR 9", 
+        peso: 6.2 },
+    { 
+        nome: "Cannondale SuperSix EVO Lab71", 
+        peso: 6.8 
+    },
+    { 
+        nome: "Pinarello Dogma F12", 
+        peso: 6.9 
+    },
+    { 
+        nome: "Canyon Ultimate CFR", 
+        peso: 6.3 
+    },
+    { 
+        nome: "Colnago V4Rs", 
+        peso: 6.8 
+    },
+    { 
+        nome: "Giant TCR Advanced SL 0", 
+        peso: 6.4 },
+    { 
+        nome: "Wilier Filante SLR", 
+        peso: 6.8 },
+    { 
+        nome: "Bianchi Specialissima RC", 
+        peso: 6.6 
+    },
+    { 
+        nome: "Scott Addict RC Ultimate", 
+        peso: 6.7 
+    }
+];
