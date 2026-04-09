7f5538755e842be077533d83f59b4141f2fce973
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-09 16:07:38 +0200

    ADD array[{nomi}, {falli}

--- script2.js
+++ script2.js
@@ -39,9 +39,21 @@ function getRandomInt(min, max) {
 
 // Ciclo for x5 per modificare punti e falli
 for (let i = 0; i < 5; i++) {
-    console.log(squadreCalcio[i].puntiFatti = getRandomInt(0, 10));
-    console.log(squadreCalcio[i].falliSubiti = getRandomInt(0, 10));
+    squadreCalcio[i].puntiFatti = getRandomInt(0, 5);      // Per ogni [i] di punti fatti genero un numero a caso da 0 a 5 
+    squadreCalcio[i].falliSubiti = getRandomInt(0, 10);    // Per ogni [i] di falli subiti genero un numero a caso da 0 a 10 
     
 }
+console.log(squadreCalcio);
+
 
 // - Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+const squadreFalli = [];
+
+for (const squadreTemp of squadreCalcio) {
+    squadreFalli.push({
+        nome: squadreTemp.nome,
+        falliSubiti: squadreTemp.falliSubiti
+    });
+}
+
+console.log(squadreFalli);
